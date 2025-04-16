61259d644c800351691c7c1595f596bb101ea09b
phuongbao90 <phuongbao90@gmail.com>
2025-04-16 12:08:25 +0700

    chore: remove unstarted apps

--- apps/dashboard/layouts/default.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <h1>Default Layout</h1>
-    <AppHeader />
-    <slot />
-    <!-- <AppFooter /> -->
-  </div>
-</template>
--- apps/dashboard/lib/api.ts
+++
@@ -1,8 +0,0 @@
-export const api = $fetch.create({
-  baseURL: "http://localhost:3000",
-  headers: {
-    // Authorization: `Bearer ${token}`,
-  },
-});
-
-export function setAuthToken(token: string) {}
